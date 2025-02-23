3af92d85428b6ec01e66296b680f6cc0ac6b0cab
davidyoum <davidyoum@yahoo.com>
2025-02-23 17:34:29 -0500

    Testing

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ bot = commands.Bot(intents=intents, command_prefix='!')
 
 @bot.event
 async def on_ready():
-    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='puddles form'))
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='puddless form'))
     await setup(bot)
     await bot.tree.sync()
 
