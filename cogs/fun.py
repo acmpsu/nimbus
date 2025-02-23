d7b46a4346549c6213f5f4d8b7fb9e1c790a35be
Blizzero <invictusblizzero@gmail.com>
2025-02-23 17:18:32 -0500

    usage restrictions

--- cogs/fun.py
+++ cogs/fun.py
@@ -3,11 +3,20 @@ import os
 from discord.ext import commands
 
 
+WHITELISTED_CHANNELS = [1343099049473212559, 1326585585741074514]
+
+
 class Fun(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    def whitelisted_channel():
+        def predicate(ctx):
+            return ctx.channel.id in WHITELISTED_CHANNELS
+        return commands.check(predicate)
+
     @commands.hybrid_command()
+    @commands.check_any(whitelisted_channel(), commands.has_permissions(administrator=True))
     async def cat(self, ctx):
         response = requests.get(
             'https://api.thecatapi.com/v1/images/search', params='api_key='+os.getenv('CAT_API_KEY'))
